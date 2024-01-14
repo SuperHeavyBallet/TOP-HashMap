3b0ff4be2a7ac390f5e3e91024e0ea51beebe87c
Alexander Gorham <agram@hotmail.co.uk>
2024-01-14 04:03:07 +0100

    Set up HashMap Project

--- js.js
+++ js.js
@@ -24,7 +24,7 @@ class HashMap
 {
     constructor()
     {
-
+        this.data = {};
     }
 
     hash(inputString)
@@ -45,52 +45,95 @@ class HashMap
 
     set(key, value)
     {
+        this.data[key] = value;
 
     }
 
     get(key)
     {
-
+        return this.data[key];
     }
 
     has(key)
     {
-
+        return key in this.data;
     }
 
-    remove()
+    remove(key)
     {
+
+        if (this.has(key)) {
+            delete this.data[key];
+            return true; // indicates succesful deletion
+        }
+        else
+        {
+            return false; // key not found
+        }
 
     }
 
     length()
     {
-
+        return Object.keys(this.data).length;
     }
 
     clear()
     {
+        
+        const keys = Object.keys(this.data);
+
+        for (const key of keys)
+        {
+            delete this.data[key];
+        }
 
     }
 
     keys()
     {
-
+        return Object.keys(this.data);
     }
 
     values()
     {
-
+        return Object.values(this.data);
     }
 
     entries()
     {
-        
+        return Object.entries(this.data);
     }
 
 }
 
 const newHash = new HashMap; 
-const nameToHash = "Brian";
 
-console.log(`${nameToHash}: ` , newHash.hash(nameToHash));
+//Initialise Keys
+const brian =  newHash.hash("brian");
+const jeff =  newHash.hash("jeff");
+
+console.log("// Set / Get Function //");
+newHash.set(brian, "London");
+console.log(newHash.get(brian));
+
+newHash.set(jeff, "Canada");
+console.log(newHash.get(jeff));
+
+console.log("// Has Function //");
+console.log(newHash.has(brian));
+
+console.log("// Remove Function //");
+//console.log(newHash.remove(brian));
+console.log(newHash.remove(42));
+
+console.log("// Entries Function //");
+console.log(newHash.entries());
+
+console.log("// Length and Clear Functions");
+console.log(newHash.length());
+newHash.clear();
+console.log(newHash.length());
+
+
+
